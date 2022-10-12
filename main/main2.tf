035c33dce8f0ffde939be02bca392e0b31dedba7
vamsireddy <vamsireddy@vamsireddys-MacBook-Pro.local>
2022-10-12 17:21:27 +0100

    third day exercise

--- main/main2.tf
+++ main/main2.tf
@@ -2,17 +2,13 @@ provider "azurerm"{
     features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-    name      = var.rg_name
-    location  = var.location
-    tags      = var.tags
+module "resource_group" {
+    source = "../modules/resourcegroup"
 }
 
-resource "azurerm_storage_account" "storage_account" {
-  name                     = var.st_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = var.account_tier
-  account_replication_type = var.account_replication_type
-  tags                     = var.tags
+module "storage_account" {
+    source = "../modules/storageaccount"
+    depends_on = [module.resource_group]
+    resource_group_name = module.resource_group.resource_group_name
 }
+
